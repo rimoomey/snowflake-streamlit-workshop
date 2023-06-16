8a4d789bfe59daf9702fa456f0ebcb3d3f401d5e
Rimon <rimon.moomey@gmail.com>
2023-06-16 14:14:08 -0500

    feat: add text entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruit = 'kiwi'
+fruit = ''
+fruit_choice = streamlit.text_input("What fruit would you like information about?", fruit)
+streamlit.write('The user entered', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
