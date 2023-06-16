dc8e39ac7e369796f2ec5a3e82bcbf71876ed64f
Rimon <rimon.moomey@gmail.com>
2023-06-16 14:12:38 -0500

    feat: add query variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit = 'kiwi'
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
