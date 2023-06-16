4c49e27df2298a62f7f6fac761eb64daf60b36aa
Rimon <rimon.moomey@gmail.com>
2023-06-16 15:25:00 -0500

    feat: close connections after queries

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,9 +48,12 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 streamlit.header("The fruit load list contains:")
 if streamlit.button("Get Fruit Load List"):
 	my_data_rows = get_fruit_load_list()
+	my_cnx.close()
 	streamlit.dataframe(my_data_rows)
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
 if streamlit.button("Add a Fruit to the List"):
 	back_from_function = insert_row_snowflake(add_my_fruit)
+	my_cnx.close()
 	streamlit.write(back_from_function)
